f2ebf2818700666adcd2f477cd94f3eeabbde6ed
manavmehta0018 <98983003+manavmehta0018@users.noreply.github.com>
2022-02-08 22:56:44 +0530

    Add files via upload

---
+++ DVM_MANAV_MINESWEEPER/Minesweeper macOS/AppDelegate.swift
@@ -0,0 +1,24 @@
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ DVM_MANAV_MINESWEEPER/Minesweeper macOS/GameViewController.swift
@@ -0,0 +1,24 @@
+
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
